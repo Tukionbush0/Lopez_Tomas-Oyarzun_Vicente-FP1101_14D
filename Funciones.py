e97fc3873b53fdb805d8ad8b2d88e9c8f51f11be
vicenteoyarzun <vi.oyarzuns@duocuc.cl>
2024-06-24 09:16:20 -0400

    Funcion agregar a lista

---
+++ Funciones.py
@@ -0,0 +1,7 @@
+def agregar_libro(lista_libros):
+    nombre=input("Ingrese nombre del libro=")
+    autor=input("Ingrese autor del libro=")
+    año=input("Ingrese año de publicacion=")
+    genero=input("Ingrese el genero del libro=")
+    lista_libros[[nombre,autor,año,genero]]
+
