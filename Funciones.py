82fde87a7e153c3f345f5e4d2f527e1e55ef84df
Tukionbush0 <to.lopezf@duocuc.cl>
2024-06-24 10:05:50 -0400

    #ARREGLO MENU

--- Funciones.py
+++ Funciones.py
@@ -1,29 +1,39 @@
 def agregar_libro(lista_libros):
-    numero=input("Asigna un numero=")
+    numero=1
     nombre=input("Ingrese nombre del libro=")
     autor=input("Ingrese autor del libro=")
     año=input("Ingrese año de publicacion=")
     genero=input("Ingrese el genero del libro=")
     lista_libros[[numero,nombre,autor,año,genero]]
+    numero=numero+1
 def ver_libros(lista_libros):
     for i in lista_libros:
         print(i)
-
-
-
-
-
-
-
-
 def modificar_libro(lista_libros):
-    nombre=input("Ingrese nombre del libro=")
-    autor=input("Ingrese autor del libro=")
-    año=input("Ingrese año de publicacion=")
-    genero=input("Ingrese el genero del libro=")
+    numeromodificar=int(input("Ingrese el numero del libro que desea modificar"))
+    if numeromodificar in lista_libros:
+     nombre=input("Ingrese nombre del libro=") 
+     lista_libros.append[numeromodificar][1]=nombre
+     autor=input("Ingrese autor del libro=")
+     lista_libros.append[numeromodificar][2]=autor
+     año=input("Ingrese año de publicacion=")
+     lista_libros.append[numeromodificar][3]=año
+     genero=input("Ingrese el genero del libro=")
+     lista_libros.append[numeromodificar][4]=genero
 
 def eliminar_libro(lista_libros):
-    nombre=input("Ingrese nombre del libro=")
-    autor=input("Ingrese autor del libro=")
-    año=input("Ingrese año de publicacion=")
-    genero=input("Ingrese el genero del libro=")
+    numeroeliminar=int(input("Ingrese numero de libro que quiere modificar"))
+    if numeroeliminar in lista_libros:
+        lista_libros.remove[[numeroeliminar]]
+
+def menu_ops(lista_libros):
+    while True:
+     print("1.- Agregar Libro")
+     print("2.- Ver todos los libros")
+     print("3.- Modificar un libro")
+     print("4.- Eliminar un Libro")
+     print("5.- Guardar coleccion en un archivo")
+     print("6.- Salir del programa")
+     print("")
+     input("ingrese una opción: ")
+     break
--- Main.py
+++ Main.py
@@ -1,15 +1,6 @@
-import Funciones as Biblioteca
+import Funciones as biblioteca
+
 
 #Listas para la biblioteca
 lista_libros=[["numero","Nombre","Autor","Año de publicacion","Genero"]]
-#Menu de opciones
-while True:
- print("1.- Agregar Libro")
- print("2.- Ver todos los libros")
- print("3.- Modificar un libro")
- print("4.- Eliminar un Libro")
- print("5.- Guardar coleccion en un archivo")
- print("6.- Salir del programa")
- print("")
- input("ingrese una opción: ")
- break
+
