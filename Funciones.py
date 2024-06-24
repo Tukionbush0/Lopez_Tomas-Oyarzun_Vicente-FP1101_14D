a16d08f15e2f3c90de1f9af4d6a0000d07578254
vicenteoyarzun <vi.oyarzuns@duocuc.cl>
2024-06-24 09:50:17 -0400

    eliminar libros

--- Funciones.py
+++ Funciones.py
@@ -1,14 +1,14 @@
 def agregar_libro(lista_libros):
-    numero=input("Asigna un numero=")
+    numero=1
     nombre=input("Ingrese nombre del libro=")
     autor=input("Ingrese autor del libro=")
     año=input("Ingrese año de publicacion=")
     genero=input("Ingrese el genero del libro=")
     lista_libros[[numero,nombre,autor,año,genero]]
+    numero=numero+1
 def ver_libros(lista_libros):
     for i in lista_libros:
         print(i)
-
 
 
 
@@ -23,7 +23,6 @@ def modificar_libro(lista_libros):
     genero=input("Ingrese el genero del libro=")
 
 def eliminar_libro(lista_libros):
-    nombre=input("Ingrese nombre del libro=")
-    autor=input("Ingrese autor del libro=")
-    año=input("Ingrese año de publicacion=")
-    genero=input("Ingrese el genero del libro=")
+    numeroeliminar=int(input("Ingrese numero de libro que quiere modificar"))
+    if numeroeliminar in lista_libros:
+        lista_libros.remove[[numeroeliminar]]
