6ec342fe0f51605b07729b88c0d1bea073a28251
Tukionbush0 <to.lopezf@duocuc.cl>
2024-06-24 09:20:05 -0400

    #while true,break

--- Main.py
+++ Main.py
@@ -1,12 +1,13 @@
 import Funciones as Biblioteca
 
 #Menu de opciones
-
-print("1.- Agregar Libro")
-print("2.- Ver todos los libros")
-print("3.- Modificar un libro")
-print("4.- Eliminar un Libro")
-print("5.- Guardar coleccion en un archivo")
-print("6.- Salir del programa")
-print("")
-input("ingrese una opción: ")
+while True:
+ print("1.- Agregar Libro")
+ print("2.- Ver todos los libros")
+ print("3.- Modificar un libro")
+ print("4.- Eliminar un Libro")
+ print("5.- Guardar coleccion en un archivo")
+ print("6.- Salir del programa")
+ print("")
+ input("ingrese una opción: ")
+ break
