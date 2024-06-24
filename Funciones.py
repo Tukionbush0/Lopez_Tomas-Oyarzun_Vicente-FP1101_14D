8789418f332dfde5e838d5fbd8fbd72fdcf2d9cb
Tukionbush0 <to.lopezf@duocuc.cl>
2024-06-24 09:24:36 -0400

    #prototipo de fuciones

--- Funciones.py
+++ Funciones.py
@@ -5,3 +5,26 @@ def agregar_libro(lista_libros):
     genero=input("Ingrese el genero del libro=")
     lista_libros[[nombre,autor,año,genero]]
 
+
+
+
+
+
+
+
+
+
+
+
+
+def modificar_libro(lista_libros):
+    nombre=input("Ingrese nombre del libro=")
+    autor=input("Ingrese autor del libro=")
+    año=input("Ingrese año de publicacion=")
+    genero=input("Ingrese el genero del libro=")
+
+def eliminar_libro(lista_libros):
+    nombre=input("Ingrese nombre del libro=")
+    autor=input("Ingrese autor del libro=")
+    año=input("Ingrese año de publicacion=")
+    genero=input("Ingrese el genero del libro=")
