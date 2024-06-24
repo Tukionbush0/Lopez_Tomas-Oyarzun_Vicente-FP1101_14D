e86038c50355f19645ecb7aefc4b6d3fbc0a4f2a
vicenteoyarzun <vi.oyarzuns@duocuc.cl>
2024-06-24 10:24:20 -0400

    Menu

--- Main.py
+++ Main.py
@@ -1,20 +1,7 @@
-import Funciones as Biblioteca
+import Funciones as biblioteca
+
 
 #Listas para la biblioteca
 lista_libros=[["numero","Nombre","Autor","Año de publicacion","Genero"]]
-#probando que funcione
-Biblioteca.agregar_libro
-Biblioteca.ver_libros
-Biblioteca.modificar_libro
-Biblioteca.eliminar_libro
-#Menu de opciones
-while True:
- print("1.- Agregar Libro")
- print("2.- Ver todos los libros")
- print("3.- Modificar un libro")
- print("4.- Eliminar un Libro")
- print("5.- Guardar coleccion en un archivo")
- print("6.- Salir del programa")
- print("")
- input("ingrese una opción: ")
- break
+
+biblioteca.menu_ops(lista_libros)
